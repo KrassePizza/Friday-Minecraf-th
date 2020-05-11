78bc3ec4d21266b3d940fae1064d7e7363c646d1
KrassePizza <65123487+KrassePizza@users.noreply.github.com>
2020-05-11 15:53:44 +0200

    Add files via upload

---
+++ java/com/krassepizza/fridaymod/items/tools/ToolSword.java
@@ -0,0 +1,28 @@
+package com.krassepizza.fridaymod.items.tools;
+
+import com.krassepizza.fridaymod.Main;
+import com.krassepizza.fridaymod.init.ModItems;
+import com.krassepizza.fridaymod.util.IHasModel;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemSword;
+
+public class ToolSword extends ItemSword implements IHasModel
+{
+	public ToolSword(String name, ToolMaterial material)
+	{
+		super(material);
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(Main.FridaymodTAB);
+		
+		ModItems.ITEMS.add(this);
+	}
+
+	@Override
+	public void registerModels() 
+	{
+		Main.proxy.registerItemRenderer(this, 0, "inventory");
+		
+	}
+}
